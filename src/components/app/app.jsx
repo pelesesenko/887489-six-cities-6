1ac0dd2d7d9f1e4fdafb8cfc7dde96cffee8df5b
pelesesenko <kw04@i.ua>
2021-02-05 14:32:39 +0300

    module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
-import MainPage from '../main-page/main-page';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import Main from '../page-main/page-main';
+import SignIn from '../page-sign-in/page-sign-in';
+import Favorites from '../page-favorites/page-favorites';
+import Room from '../page-room/page-room';
 import PropTypes from 'prop-types';
 
 const App = (props) => {
   const {offersAmountToShow} = props;
   return (
-    <MainPage offersAmountToShow={offersAmountToShow} />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" exact component={SignIn}/>
+        <Route path="/favorites" exact component={Favorites}/>
+        <Route path="/offer/:id?" exact component={Room}/>
+        <Route path="/" exact >
+          <Main offersAmountToShow={offersAmountToShow} />
+        </Route>
+        <Route
+          render={() => (
+            <>
+              <h1>
+                404.
+                <br />
+                <small>Page not found</small>
+              </h1>
+              <Link to="/">Go to main page</Link>
+            </>
+          )}
+        />
+        {/* <Route>
+          <>
+            <h1>
+              404.
+              <br />
+              <small>Page not found</small>
+            </h1>
+            <Link to="/">Go to main page</Link>
+          </>
+        </Route> */}
+      </Switch>
+    </BrowserRouter>
   );
 };
 
--- src/components/offer-card/offer-card.jsx
+++ src/components/offer-card/offer-card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const OfferCard = () => {
 
@@ -8,9 +9,9 @@ const OfferCard = () => {
         <span>Premium</span>
       </div>
       <div className="cities__image-wrapper place-card__image-wrapper">
-        <a href="#">
+        <Link to="/offer">
           <img className="place-card__image" src="img/apartment-01.jpg" width={260} height={200} alt="Place image" />
-        </a>
+        </Link>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
@@ -32,7 +33,7 @@ const OfferCard = () => {
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+          <Link to="/offer">Beautiful &amp; luxurious apartment at great location</Link>
         </h2>
         <p className="place-card__type">Apartment</p>
       </div>
