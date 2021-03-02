08c5c7a2f59ceab8cd8a6b466f92aade29d37dbc
pelesesenko <kw04@i.ua>
2021-03-02 18:20:03 +0300

    module7-task2

--- src/components/page-main/page-main.jsx
+++ src/components/page-main/page-main.jsx
@@ -14,6 +14,7 @@ import CitiesList from '../cities-list/cities-list';
 import SortSelect from '../sort-select/sort-select';
 import OffersList from '../offers-list/offers-list';
 import Map from '../map/map';
+import MainEmpty from './main-empty';
 
 import PropTypes from 'prop-types';
 import {hotelsPropTypes} from '../../prop-types';
@@ -57,15 +58,7 @@ const PageMain = (props) => {
             <div className="cities">
               {!offersInCity.length
                 ?
-                <div className="cities__places-container cities__places-container--empty container">
-                  <section className="cities__no-places">
-                    <div className="cities__status-wrapper tabs__content">
-                      <b className="cities__status">No places to stay available</b>
-                      <p className="cities__status-description">We could not find any property available at the moment in {currentCityName}</p>
-                    </div>
-                  </section>
-                  <div className="cities__right-section" />
-                </div>
+                <MainEmpty/>
                 :
                 <div className="cities__places-container container">
                   <section className="cities__places places">
